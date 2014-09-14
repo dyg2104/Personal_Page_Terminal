8b067e103b40857d0953c3498120ea0a1d60fb6d
dyg2104 <dyg2104@gmail.com>
2014-09-13 22:04:12 -0400

    Allows lowercase or uppercase inputs

--- app/assets/javascripts/views/bar.js
+++ app/assets/javascripts/views/bar.js
@@ -17,13 +17,13 @@ PersonalPageTerminal.Views.Bar = Backbone.View.extend({
     event.preventDefault();
     var entered = $(".bar-input-form").serializeJSON();
 
-    if (entered.command === "cd Bio") {
+    if (entered.command === "cd Bio" || entered.command === "cd bio") {
       var bioView = new PersonalPageTerminal.Views.Bio();
       $(document).find("#prepend-with-content").append(bioView.render().$el);
-    } else if (entered.command === "cd Resume") {
+    } else if (entered.command === "cd Resume" || entered.command === "cd resume") {
       var resumeView = new PersonalPageTerminal.Views.Resume();
       $(document).find("#prepend-with-content").append(resumeView.render().$el);
-    } else if (entered.command === "cd Projects") {
+    } else if (entered.command === "cd Projects" || entered.command === "cd projects") {
       var projectsView = new PersonalPageTerminal.Views.Projects();
       $(document).find("#prepend-with-content").append(projectsView.render().$el);
     } else if (entered.command === "ls") {
