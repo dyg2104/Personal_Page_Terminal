2bf3b315862fa41eec05ae7c37c0f1c719d1caad
dyg2104 <dyg2104@gmail.com>
2014-09-13 21:44:21 -0400

    Removes empty folders

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,8 +18,6 @@
 //= require backbone
 //= require personal_page_terminal
 //= require_tree ../templates
-//= require_tree ./models
-//= require_tree ./collections
 //= require_tree ./views
 //= require_tree ./routers
 //= require_tree .
